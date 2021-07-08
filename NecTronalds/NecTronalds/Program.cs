d59a8121129cf176cf7048209bc6e80b0b89e18c
fernandonakagawa <fernando.nakagawa@sesisenaipr.org.br>
2021-07-08 19:28:21 -0300

    Criação do projeto.

---
+++ NecTronalds/NecTronalds/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NecTronalds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
